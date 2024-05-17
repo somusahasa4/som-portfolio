d788b5c43ae97cf5e16065bd4030e57cad055756
somusahasa4 <somusahasa4@gmail.com>
2024-05-17 12:33:33 +0530

    Remove lorem ipsum and use gh-pages

--- src/Components/Techknowledge.js
+++ src/Components/Techknowledge.js
@@ -14,28 +14,28 @@ const Techknowledge = () => (
           <div className="icon-box">
           <div className="icon acc-icon"></div>
           <h4 className="title">Adobe Campaign Classic</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon acs-icon"></div>
           <h4 className="title">Adobe Campaign Standard</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon aep-icon"></div>
           <h4 className="title">Adobe Experience Platform</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon aa-icon"></div>
           <h4 className="title">Adobe Analytics</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           </div>
@@ -48,28 +48,28 @@ const Techknowledge = () => (
           <div className="icon-box">
           <div className="icon html-icon"></div>
           <h4 className="title">HTML</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon css-icon"></div>
           <h4 className="title">CSS</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon js-icon"></div>
           <h4 className="title">Javascript</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon react-icon"></div>
           <h4 className="title">ReactJs</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           </div>
@@ -82,28 +82,28 @@ const Techknowledge = () => (
           <div className="icon-box">
           <div className="icon bs-icon"></div>
           <h4 className="title">Bootstrap</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon jq-icon"></div>
           <h4 className="title">Jquery</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon soap-icon"></div>
           <h4 className="title">Soap API</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           <div className="col-md-6 col-lg-3 align-items-stretch mb-lg-0">
           <div className="icon-box">
           <div className="icon rest-icon"></div>
           <h4 className="title">Rest API</h4>
-          <p className="description">Lorem ipsum</p>
+          
           </div>
           </div>
           </div>
