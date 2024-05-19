1b6ba198202a491dc155e4461c3a2e8aec9c3e88
somusahasa4 <somusahasa4@gmail.com>
2024-05-19 12:34:23 +0530

    Add json data for Contact me page

---
+++ src/Components/contentJson.js
@@ -0,0 +1,28 @@
+let portfolioContent = {
+    "imagebaseurl":"",
+    "name": "Somasundaram Haldurai",
+    "role": "Front End Developer and Digital Experience Professional",
+    "linkedinId":"https://somusahasa4.github.io/som-portfolio/",
+    "skypeid": "",
+    "roleDescription": "10+ years of IT experience in Web Technologies and Digital Marketing Technologies, Adobe products(Adobe Campaign Classic, Adobe Campaign Standard, Adobe Campaign v8, Adobe Experience Manager, Adobe Analytics etc). Ability to understand and work on the new proposal. Worked on multiple RFPs including demo to illustrate the capabilities of Adobe Campaign management to clients. Setting up Batch communication/Realtime communication across different channels like Email/SMS/Push",
+    "contactMeHeading": "Contact me!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://in.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://somusahasa4.github.io/som-portfolio/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ]
+  }
+  
+  export default portfolioContent
